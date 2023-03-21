6d11f0c7a32db70445546e20721d797656cf657e
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-03-21 17:51:24 +0300

    :hammer: [fix] model predict method

--- app/model.py
+++ app/model.py
@@ -5,15 +5,24 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class Model:
-    def __init__(self, d):
-        self.data = d
+    def __init__(self, game_df: pd.DataFrame):
+        self.df = game_df
+        self.initial_vector = []
 
-    def predict(self, game_id: int, top: int = 5, offset: int = 0):
-        selected_games_df = self.data.loc[self.data['App ID'].isin([game_id])].drop(columns=['App ID'])
-        m_data = self.data.drop(columns=['App ID'])
-        selected_games_union = pd.DataFrame(selected_games_df.max(axis=0)).T
-        results = cosine_similarity(m_data.values, selected_games_union.values).T[0]
-        return self.data.iloc[np.argpartition(results, -top)[-top:]]['App ID'].to_list()
+    def set_initial_pool(self, game_ids: list):
+        selected_df = self.df.loc[self.df['App ID'].isin(game_ids)].drop(columns=['App ID'])
+        self.initial_vector = selected_df.max(axis=0).values
+
+    def predict(self, game_id: int, liked: bool = True, top: int = 5, offset: int = 0):
+        data = self.df.set_index('App ID')
+        selected_game = data.loc[[game_id]].max(axis=0).values
+
+        m = 1 if liked else -1
+        self.initial_vector = (selected_game * m)
+
+        results = cosine_similarity(data.values, [self.initial_vector]).T[0]
+        top = np.argpartition(results, -top)[-top:]
+        return self.df.loc[top]['App ID'].to_list()
 
 
 if __name__ == "__main__":
