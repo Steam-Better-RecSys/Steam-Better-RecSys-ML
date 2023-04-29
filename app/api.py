5f0b564c6d8bbeb5877dc03b522113def1547235
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-04-29 16:16:03 +0300

    :hammer: [refactor] api

--- app/api.py
+++ app/api.py
@@ -2,7 +2,10 @@ import joblib
 import uvicorn
 import __main__
 import numpy as np
-from fastapi import FastAPI, Response, Request, Body
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
 from model import Model
 
 setattr(__main__, "Model", Model)
@@ -10,34 +13,40 @@ model = joblib.load("model.joblib")
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/")
 def get_home():
-    return {"health_check": "OK"}
+    """Check API status"""
+    status = {"health_check": "OK"}
+    return JSONResponse(content=jsonable_encoder(status))
 
 
-@app.get("/recommendations")
-def get_recommendations(request: Request, response: Response, game_id: int, liked: int = 1, top: int = 10, offset: int = 0):
-    predicted_vector = request.cookies.get("vector")
-    predictions = model.predict(predicted_vector, game_id, liked, top, offset)
-    response.set_cookie(key="vector", value=predictions["vector"])
-    return {"items": predictions["recs"]}
+@app.post("/recommendations")
+async def get_recommendations(
+    request: Request,
+    game_id: int = 0,
+    liked: int = 1,
+    top: int = 10,
+    offset: int = 0,
+):
+    """Get new recommendations based on a current games"""
+    request = await request.json()
+    predicted_vector = request["vector"]
+    content = model.predict(predicted_vector, game_id, liked, top, offset)
+    return JSONResponse(content=jsonable_encoder(content))
 
 
 @app.post("/selected_games")
-async def set_selected_games(request: Request, response: Response):
-    games_ids = await request.json()
-    games_ids = games_ids['games_ids']
-    vector = model.set_initial_pool(games_ids)
-    vector = np.array2string(vector.values, precision=2, separator=',', suppress_small=True)
-    predictions = model.predict(vector)
-    response.set_cookie(key="vector", value=predictions["vector"])
-    return predictions["recs"]
-
-
-@app.get("/selected_games")
-def get_selected_games(request: Request, response: Response):
-    return request.cookies.get('vector')
+async def set_selected_games(request: Request):
+    """Set selected games"""
+    request = await request.json()
+    games_ids = request["games_ids"].split(",")
+    vector = model.set_initial_vector(games_ids)
+    vector = np.array2string(
+        vector.values, precision=2, separator=",", suppress_small=True
+    )
+    content = {"vector": vector}
+    return JSONResponse(content=jsonable_encoder(content))
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='127.0.0.1', port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
